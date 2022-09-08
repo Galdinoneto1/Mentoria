32db366e6f96328d82e8ff635a269ff50fc9c462
Galdino José <galdino.neto1@hotmail.com>
2022-09-08 13:59:38 -0300

    first commit

--- galdino/Execercio/TwoSum.java
+++ galdino/Execercio/TwoSum.java
@@ -32,13 +32,13 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
+        int[] nums = new int[] { 2, 7, 11, 15 };
         int target = 9;
         System.out.println("The indices of Pair: " + Arrays.toString(twoSum(nums, target)));
 
-        nums = new int[]{3, 2, 4};
+        nums = new int[] { 3, 2, 4 };
         target = 6;
         System.out.println("The indices of Pair: " + Arrays.toString(twoSum(nums, target)));
-        Sy
+
     }
 }
