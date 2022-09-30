b5834bc2f6089e1d6be0d74551dd32aa2a659271
Galdino José <galdino.neto1@hotmail.com>
2022-09-30 16:33:04 -0300

    Alteração cod.

--- galdino/Execercio/prefix.java
+++ galdino/Execercio/prefix.java
@@ -1,6 +1,12 @@
 package galdino.Execercio;
+//Write a function to find the longest common prefix string amongst an array of strings.
+//
+//If there is no common prefix, return an empty string "".
 
 class Solution {
+    public static void main(String[] args) {
+    }
+
 
     public String prefix(String[] strs) {
         if(strs == null || strs.length == 0)    return "";
